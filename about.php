8b2d9adf05069f9e15a83f4487a82cbaf44af2b6
David Delgado <david@sidewaysthought.net>
2024-01-14 23:23:54 -0600

    Update footer and navbar links

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About This Site | Convert HTML to Markdown - Online Tool</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include('includes/head.php'); ?>
+</head>
+
+<body>
+    <?php include('includes/navbar.php'); ?>
+
+    <div class="container mt-4">
+
+        <h2>About This Site</h2>
+
+        <p>Welcome to the "HTML to Markdown Converter", a user-friendly online tool designed to simplify the process of
+            converting HTML content into Markdown. Ideal for platforms like ChatGPT or GitHub, our tool is perfect for
+            both tech-savvy individuals and those less familiar with HTML.</p>
+        <h3>Features</h3>
+        <p>The converter is intuitive and straightforward, offering features like CSS selector-based HTML filtering, and
+            options to copy or download the Markdown output.</p>
+
+        <h3>Technologies Used</h3>
+        <p>This site is built with PHP and Bootstrap, a framework developed by (the company formally known as) Twitter.
+            which helps ensure a responsive and mobile-first experience. The core functionality, converting HTML to
+            Markdown, is made possible by <a href="https://github.com/mixmark-io/turndown">TurnDown</a>, a versatile
+            JavaScript library which is copyright 2017 by Dom CHristie, and is released under the MIT license.</p>
+
+        <h3>Terms of Use</h3>
+            The tool is completely free to use. Your data is neither stored nor used beyond the conversion process. <a
+            href="terms.php">Read the full Terms of Use here</a>. <a href="privacy.php">Read the full Privacy
+            Policy</a>.
+
+        <h3>Contact Us</h3>
+            For feedback, support, or inquiries, email <a
+            href="mailto:info@sidewaysthought.net">info@sidewaysthought.net</a>.
+    </div>
+
+    <?php include('includes/footer.php'); ?>
+    <?php include('includes/footer-js.php'); ?>
+
+</body>
+
+</html>
--- includes/navbar.php
+++ includes/navbar.php
@@ -3,5 +3,6 @@
             <h1 class="navbar-brand mb-0 h1">
             <a href="/index.php" class="text-white">HTML to Markdown Converter</a>
             </h1>
+            <a href="about.php" class="text-white">About</a>
         </div>
     </nav>
