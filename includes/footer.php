c99a8d6be2b5ff1954972f8e0c8f58f31851b7f7
David Delgado <david@sidewaysthought.net>
2024-01-15 18:28:48 -0600

    Add support for Ko-fi widget in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,22 @@
     <footer class="footer mt-auto py-3 bg-dark text-white mt-4">
         <div class="container">
-            <p>
-                &copy; 2024 <a href="https://www.sidewaysthought.net" class="text-white">Sideways Thought LLC</a>.
-                    All rights reserved. See the About page for tools and frameworks used in this project.<br>
-                <a href="about.php" class="text-white">About</a> | <a href="terms.php" class="text-white">Terms of Use</a> | <a href="privacy.php" class="text-white">Privacy Policy | <a href="mailto:info@sidewaysthought.net" class="text-white">Contact</a>
-            </p>
-            <p>✨The content of this website was created with the assistance of generative AI</p>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>
+                        &copy; 2024 <a href="https://www.sidewaysthought.net" class="text-white">Sideways Thought LLC</a>.
+                            All rights reserved. See the About page for tools and frameworks used in this project.<br>
+                        <a href="about.php" class="text-white">About</a> | <a href="terms.php" class="text-white">Terms of Use</a> | <a href="privacy.php" class="text-white">Privacy Policy | <a href="mailto:info@sidewaysthought.net" class="text-white">Contact</a>
+                    </p>
+                    <p>✨The content of this website was created with the assistance of generative AI</p>
+                </div>
+                <div class="col-md-6">
+                    <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script>
+                    <script type='text/javascript'>
+                        kofiwidget2.init('Support Me on Ko-fi', '#198754', 'G2G2TDET6');
+                        kofiwidget2.draw();
+                    </script> 
+                </div>
+            </div>
+
         </div>
     </footer>
